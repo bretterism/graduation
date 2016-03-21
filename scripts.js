319f7c35f7f178e0fe63b16a214ad88830693970
bretterism <bretterism@gmail.com>
2016-03-21 16:32:36 -0700

    Finished rsvp template.

--- scripts.js
+++ scripts.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
-    // init controller
-	var controller = new ScrollMagic.Controller();
+	$("[name='attending-ceremony']").bootstrapSwitch({
+		onText: 'yes',
+		offText: 'no',
+		size: 'mini'
+	});
+	$("[name='attending-open-house']").bootstrapSwitch({
+		onText: 'yes',
+		offText: 'no',
+		size: 'mini'
+	});
+ //    // init controller
+	// var controller = new ScrollMagic.Controller();
 
-	// create a scene
-	new ScrollMagic.Scene({
-	        duration: 100,  // the scene should last for a scroll distance of 100px
-	        offset: 50      // start this scene after scrolling for 50px
-	    })
-	    .setPin("#my-sticky-element") // pins the element for the the scene's duration
-	    .addTo(controller); // assign the scene to the controller
+	// // create a scene
+	// new ScrollMagic.Scene({
+	//         duration: 100,  // the scene should last for a scroll distance of 100px
+	//         offset: 50      // start this scene after scrolling for 50px
+	//     })
+	//     .setPin("#my-sticky-element") // pins the element for the the scene's duration
+	//     .addTo(controller); // assign the scene to the controller
 });
